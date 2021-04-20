901381a491d5791c34dbe3a4b7c28eb74be2a252
Nathan Pham <nathanpham.me@gmail.com>
2021-04-20 08:33:47 -0700

    gitignore & server

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const path = require("path")
+
+const app = express()
+
+app.use(express.static(path.resolve(__dirname, "static")))
+
+app.get("/*", (req, res) => {
+    res.sendFile(path.resolve("static", "index.html"))
+})
+
+app.listen(8080, () => {
+    console.log("server is live")
+})
